9cec504747a3c61823f9a37d755d3704e024f7d1
bunnyisadevil <86530239+bunnyisadevil@users.noreply.github.com>
2021-06-26 22:34:11 +0530

    Update app.js

--- Web Panel/app.js
+++ Web Panel/app.js
@@ -1,15 +1,17 @@
 ///Edit Under This only
 
   
-    var firebaseConfig = {
-    apiKey: "API KEY",
-    authDomain: "AUTH DOMAIN",
-    databaseURL: "DATABSE URL",
-    projectId: "PROJECT ID",
-    storageBucket: "BUCKET URL",
-    messagingSenderId: "SENDER ID",
-    appId: "APP ID"
-  }; 
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA6qfXYmGL8l58wRILVf6JZ8Hjqc9dYCXE",
+  authDomain: "test-49a7d.firebaseapp.com",
+  databaseURL: "https://test-49a7d-default-rtdb.firebaseio.com",
+  projectId: "test-49a7d",
+  storageBucket: "test-49a7d.appspot.com",
+  messagingSenderId: "778742588393",
+  appId: "1:778742588393:web:b48bc5a555c473e5c86178",
+  measurementId: "G-CY8NZNMDWJ"
+};
  
 ////Do Not Edit Below This
 
